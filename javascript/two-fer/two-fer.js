cbd1d1598a898d6acca16a296a14dd96575c2db2
Anthony Morris <anthony@amorrissound.com>
2020-02-14 07:57:34 -0800

    Use default param

--- javascript/two-fer/two-fer.js
+++ javascript/two-fer/two-fer.js
@@ -3,6 +3,6 @@
 // convenience to get you started writing code faster.
 //
 
-export const twoFer = (name) => {
-  return `One for ${name || 'you'}, one for me.`;
+export const twoFer = (name = 'you') => {
+  return `One for ${name}, one for me.`;
 };
